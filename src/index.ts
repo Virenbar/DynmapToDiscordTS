7bac9b2130c7dd01c54e330beaee17406a040f70
Artyom <rib.artem@gmail.com>
2020-11-30 23:26:39 +0500

    Show config info on start

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { configure, getLogger } from "log4js"
 import { sendMessage } from './lib/discord'
-import config from "./lib/config"
+import loadConfig, { config } from "./lib/config"
 import Server from './modules/serverInfo'
 import Dynmap from './modules/dynmapInfo'
 
-config()
+loadConfig()
 configure({
     appenders: {
         file: { type: "file", filename: "logs/debug.log", maxLogSize: 1024 * 1024 * 10, backups: 5, compress: true },
@@ -17,13 +17,13 @@ configure({
 });
 export const logger = getLogger('DtD')
 const name = 'Dynmap to Discord'
-const version = '4.0'
+const version = '4.1'
 
 async function Init() {
     logger.info('Starting')
     await sendMessage({
         "title": name,
-        "message": 'Version: ' + version,
+        "message": `Version: ${version}\nServer: ${config.host}:${config.port}\nDynmap: ${config.dynmap}`,
         "timestamp": new Date().toISOString()
     })
     await Server()
